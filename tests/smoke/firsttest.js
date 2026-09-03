210d65705f8eedbb36a217c74300d247ee1752d1
Ashish.rai <ashishappnox1@gmail.com>
2026-09-03 13:15:30 +0530

    Add initial configuration, test scripts, and result files for performance testing

---
+++ tests/smoke/firsttest.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 1,
+    duration: '10s',
+};
+
+export default function () {
+
+    const response = http.get('https://mydentaltouch.mwstraining.com/');
+
+    check(response, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
+}
